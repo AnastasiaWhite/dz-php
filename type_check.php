c99dea9191025e3a1067d484a95b37508078e435
Streltsova <askobina3@gmail.com>
2025-09-20 20:40:08 +0700

    second dz

---
+++ type_check.php
@@ -0,0 +1,15 @@
+   <?php
+   // Примеры переменных
+   $variable = 3.14;
+   // $variable = 1;
+   // $variable = 'one';
+   // $variable = true;
+   // $variable = null;
+   // $variable = [];
+
+   // Определение типа переменной
+   if (is_bool($variable)) {
+       $type = 'bool';
+   } elseif (is_float($variable)) {
+       $
+   }
